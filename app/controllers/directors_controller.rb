7bce9b653ea069f8cda675d0a646f86e641dde00
Satoru Nabeshima <snabeshi@chicagobooth.edu>
2023-05-16 17:18:39 +0000

    Director details halfway

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,11 +5,21 @@ class DirectorsController < ActionController::Base
     render({ :template => "directors_templates/index.html.erb"})
   end
 
-  def details
-    @director_id = params.fetch("director_id")
-    @list_of_directors = Director.all
+  def director_details
+    director_id = params.fetch("director_id")
+    @director = Director.where({:id => director_id}).at(0)
     render({ :template => "directors_templates/details.html.erb"})
-
   end
+
+  def eldest
+    @eldest = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+    render({ :template => "directors_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.order({:dob => :desc}).at(0)
+    render({ :template => "directors_templates/youngest.html.erb"})
+  end
+
 
 end
